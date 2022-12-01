87385d399d8f0d33770973a4a17c6cc20ff79262
EvaChen <82190631+evachen20041010@users.noreply.github.com>
2022-12-01 17:31:27 +0800

    Add files via upload

---
+++ 20221201/HW_13/HW_13.ino
@@ -0,0 +1,83 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+//#include <SimpleDHT.h>
+
+char SSID[] = "WY1010";
+char PASSWORD[] = "20041010311";
+String Linetoken = "0yOTW4szAcVRya1cLOjQmYQb5BguMMVHHGMbX41YlkO";
+WiFiClientSecure client;//網路連線物件
+char host[] = "notify-api.line.me";//LINE Notify API網址
+int sensorPin = A0;
+int sensorValue = 0;
+int Data = 2, Data_old = 0;
+String message;
+
+void setup() {
+  Serial.begin(115200);
+  //連線到指定的WiFi SSID
+  Serial.print("Connecting Wifi: ");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  //連線成功，顯示取得的IP
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  IPAddress ip = WiFi.localIP();
+  Serial.println(ip);
+  client.setInsecure();//ESP32核心 1.0.6以上
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  if (sensorValue < 1500) {
+    Data = 0;
+  } else if (sensorValue > 1500) {
+    Data = 1;
+  }
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  if (sensorValue < 1500 && Data == 0) {
+    message = "Good night!";
+    upload();
+    Data = 1;
+  } else if (sensorValue > 1500 && Data == 1) {
+    message = "Good morning!";
+    upload();
+    Data = 0;
+  }
+
+  //每5秒讀取一次溫濕度
+  delay(5000);
+}
+
+void upload() {
+  if (client.connect(host, 443)) {
+    int LEN = message.length();
+    //傳遞POST表頭
+    String url = "/api/notify";
+    client.println("POST " + url + " HTTP/1.1");
+    client.print("Host: "); client.println(host);
+    //權杖
+    client.print("Authorization: Bearer "); client.println(Linetoken);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: "); client.println( String((LEN + 8)) );
+    client.println();
+    client.print("message="); client.println(message);
+    client.println();
+    //等候回應
+    delay(2000);
+    String response = client.readString();
+    //顯示傳遞結果
+    Serial.println(response);
+    client.stop(); //斷線，否則只能傳5次
+  }
+  else {
+    //傳送失敗
+    Serial.println("connected fail");
+  }
+}
